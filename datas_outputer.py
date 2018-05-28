c8337b58adc855ac1fe7028f9088414be729f41f
carons-cr <2738794789@qq.com>
2018-05-28 14:36:47 +0800

    crawl 10 questions from pageOne of tag -java in CSDN's Q&A, then save it in questions.excel.

--- datas_outputer.py
+++ datas_outputer.py
@@ -18,4 +18,36 @@ class DatasOutputer(object):
 	    i = i + 1;
 	workBook.save("tags.xls");
 	
-		 
+    def write_question_sheet_head(self, workSheet):
+	workSheet.write(0, 0, "questionId");
+        workSheet.write(0, 1, "questionUrl");
+        workSheet.write(0, 2, "questionTime");
+        workSheet.write(0, 3, "questionTitle");
+        workSheet.write(0, 4, "userName");
+        workSheet.write(0, 5, "reward");
+        workSheet.write(0, 6, "existSatisfactoryAnswer");
+        workSheet.write(0, 7, "answerUrl");
+        workSheet.write(0, 8, "answerNumber");
+        workSheet.write(0, 9, "browseNumber");   
+
+    def write_question_sheet_body(self, workSheet, questions):
+	i = 1;
+        for question in questions:
+            workSheet.write(i, 0, question["questionId"]);
+            workSheet.write(i, 1, question["questionUrl"]);
+            workSheet.write(i, 2, question["questionTime"]);
+            workSheet.write(i, 3, question["questionTitle"]);
+            workSheet.write(i, 4, question["userName"]);
+            workSheet.write(i, 5, question["reward"]);
+            workSheet.write(i, 6, question["existSatisfactoryAnswer"]);
+            workSheet.write(i, 7, question["answerUrl"]);
+            workSheet.write(i, 8, question["answerNumber"]);
+            workSheet.write(i, 9, question["browseNumber"]);
+            i = i + 1; 
+
+    def output_questions(self, questions):
+	workBook = xlwt.Workbook();
+        workSheet = workBook.add_sheet("questions");
+	self.write_question_sheet_head(workSheet);
+	self.write_question_sheet_body(workSheet, questions);
+        workBook.save("questions.xls");
--- html_parser.py
+++ html_parser.py
@@ -15,34 +15,45 @@ class HtmlParser(object):
 	tag_nodes = beautiful_soup.find("div", class_="tags_list").find_all("a");
 	for tag_node in tag_nodes:
 	    new_tag = {};
-	    new_tag["tagName"] = re.search(r"[^0-9]+", tag_node.get_text()).group();
+	    new_tag["tagName"] = re.search(r"[^0-9|\s]+", tag_node.get_text()).group();
 	    new_tag["questionNumber"] = tag_node.find("em").get_text();
 	    tags.append(new_tag);
 	return tags;
+
+    def _get_data_from_question_detail_node(self, question_detail_node):
+	question_detail = {};
+        q_time_node = question_detail_node.find("div", class_="q_time");
+        question_detail["questionTime"] = re.search(r"[\d|.]+", q_time_node.find("span").get_text()).group();
+        question_detail["userName"] = q_time_node.find("a").get_text();
+        question_detail["reward"] = re.search(r"[\d|\w]+", q_time_node.find("b").find("a").get_text()).group();
+	question_node = question_detail_node.find("dl").find("dt").find("a");
+	question_detail["questionId"] = re.search(r"[\d]+", question_node["href"]).group();
+	question_detail["questionUrl"] = question_node["href"];
+        question_detail["questionTitle"] = question_node.get_text();
+        answer_node = question_detail_node.find("a", class_="answer_num");
+        question_detail["existSatisfactoryAnswer"] = answer_node["title"];
+        question_detail["answerUrl"] = answer_node["href"];
+        question_detail["answerNumber"] = answer_node.find("span").get_text();
+	return question_detail;
+
+    def _get_data_from_question_attention_node(self, question_attention_node):
+	browse_node = question_attention_node.find("em", class_="browse");
+	browseNumber = re.search(r"[0-9]+", browse_node.get_text()).group();
+	return browseNumber;
     
     def _get_questions(self, beautiful_soup):
 	questions = [];
-	question_detail_nodes = beautiful_soup.find_all("div", class_="question_detail_con");
-        for question_detail_node in question_detail_nodes:
-	    question_detail = {};
-	    q_time_node = question_detail_node.find("div", _class="q_time");
-            question_detail["questionTime"] = re.search(r"[0-9]+", q_time_node.find("span").get_text()).group();
-	    question_detail["userName"] = q_time_node.find("a").get_text();
-	    question_detail["reward"] = q_time_node.find("b").find("a").get_text();
-	    question_detail["questionTitle"] = question_detail_node.find("dl").find("dt").find("a").get_text();
-	    answer_node = question_detail_node.find("a", _class="answer_num");
-	    question_detail["existSatisfactoryAnswer"] = answer_node["title"];
-	    question_detail["answerUrl"] = answer_node["href"];
-	    question_detail["answerNumber"] = question_detail_node.find("a").find("span").get_text();
-	    questions.append(question_detail);
-	    print questions;
-	    
+	question_detail_nodes = beautiful_soup.find_all("div", class_="questions_detail_con");
 	question_attention_nodes = beautiful_soup.find_all("div", class_="share_bar_con");
-	i = 0;
+        for question_detail_node in question_detail_nodes:
+	    question_detail = self._get_data_from_question_detail_node(question_detail_node);
+	    questions.append(question_detail); 
+	question_index = 0;
         for question_attention_node in question_attention_nodes:
-	    print len(questions);
-            #questions[i]["browseNumber"] = re.search(r"[0-9]+", question_attention_node.find("em").get_text()).group();
-	    i = i + 1;
+	    browseNumber = self._get_data_from_question_attention_node(question_attention_node);
+            questions[question_index]["browseNumber"] = browseNumber;
+	    question_index = question_index + 1;
+	return questions;
 
     def parse_questions(self, html_content):
 	if html_content is None:
